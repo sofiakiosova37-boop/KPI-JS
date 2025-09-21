3e8bb68191dc49e699c65f87672de4eff5601b3a
sofiakiosova37-boop <sofiakiosova37@gmail.com>
2025-09-21 23:05:14 +0300

    Add files via upload

---
+++ 1-hoisting lab 1.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const fn = () => {
+   return n + 1;
+};
+
+module.exports = { fn };
---
+++ 2-by-value lab 1.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const inc = (obj) => {
+    obj.n = n + 1;
+    return obj;
+};
+
+module.exports = { inc };
